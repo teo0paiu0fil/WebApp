a254001df2b04b61e20f22d116779bf32cd44a74
sKill007:2222 <teofil.paiu02@gmail.com>
2023-02-15 00:47:20 +0200

    using database on server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const database = require("./database");
+const { comparePassword } = require("./hashing");
+
 
 const app = express();
 
@@ -8,9 +11,19 @@ app.use(express.json());
 
 const PORT = 8888;
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:{s}.`, PORT);
+  console.log(`Server is running at http://localhost:` + PORT);
 });
 
+app.post("/api/v1/signup", (req, res) => {
+  // todo
+});
+
+app.get("/api/v1/signin", (req, res) => {
+  database.findUser(req.body.email).then((user) => {
+      // if (user == null) return null;
+      // if(comparePassword(req.body.password, user.password))
+        res.send({ token : ''}) // todo
+  });
+});
 
